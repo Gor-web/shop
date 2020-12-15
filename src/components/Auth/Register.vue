716e31501db56933ffb81f2cb8f7ff90608d4919
Gor-web <gorhakobyan_1997@mail.ru>
2020-12-15 13:57:52 +0400

    commit changed axios

--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -2,45 +2,116 @@
   <div class="register-container ">
     <p class="text">Registration</p>
     <hr>
-    <div class="d-flex justify-content-center align-items-center ">
-      <div class="form-group w-50 ">
-        <div class="d-flex justify-content-center">
-          <input type="text" class="form-control m-2 width" placeholder="name">
+    <form @submit.prevent="submit">
+      <div class="col-sm-6 offset-sm-3">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Name"
+                 v-model.trim="$v.name.$model"
+                 :class="{'is-invalid': validationStatus($v.name)}">
+          <div v-if="!$v.name.required" class="invalid-feedback">The name is required</div>
         </div>
 
-        <div class="d-flex justify-content-center">
-          <input type="text" class="form-control m-2 width " placeholder="surname">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Surname"
+                 v-model.trim="$v.surname.$model"
+                 :class="{'is-invalid': validationStatus($v.surname)}">
+          <div v-if="!$v.surname.required" class="invalid-feedback">The surname is required</div>
         </div>
 
-        <div class="d-flex justify-content-center">
-          <input type="number" class="form-control m-2 width" placeholder="age">
+        <div class="form-group">
+          <input type="number" class="form-control" placeholder="Age"
+                 v-model.trim="$v.age.$model"
+                 :class="{'is-invalid': validationStatus($v.age)}">
+          <div v-if="!$v.age.required" class="invalid-feedback">The age is required</div>
+          <div v-if="!$v.age.maxLength" class="invalid-feedback">Age can not be more than 100 {{$v.age.$params.maxLength.min}}</div>
         </div>
 
-        <div class="d-flex justify-content-center">
-          <input type="text" class="form-control m-2 width" placeholder="email">
+        <div class="form-group">
+          <input type="text" class="form-control"
+                 placeholder="Email" :class="{'is-invalid':validationStatus($v.email)}"
+                 v-model.trim="$v.email.$model">
+          <div v-if="!$v.email.required" class="invalid-feedback">The email is required</div>
+          <div v-if="!$v.email.email" class="invalid-feedback">The email is not valid</div>
         </div>
 
-        <div class="d-flex justify-content-center">
-          <input type="text" class="form-control m-2 width" placeholder="password">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Password"
+                 :class="{'is-invalid':validationStatus($v.password)}"
+                 v-model.trim="password">
+          <div v-if="!$v.password.required" class="invalid-feedback">Password is required</div>
+          <div v-if="!$v.password.minLength" class="invalid-feedback">You mus have at least {{$v.password.$params.minLength.min}}</div>
+          <div v-if="!$v.password.maxLength" class="invalid-feedback">You mus not have a greater then {{$v.password.$params.maxLength.max}}</div>
         </div>
 
-        <div class="d-flex justify-content-center">
-          <input type="text" class="form-control m-2 width" placeholder="confirm password">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="ConfirmPassword"
+          :class="{'is-invalid': validationStatus($v.confirmPassword)}"
+          v-model.trim="confirmPassword">
+          <div v-if="!$v.confirmPassword.required" class="invalid-feedback">Confirm Password is required</div>
+          <div v-else-if="!$v.confirmPassword.sameAsPassword" class="invalid-feedback">Passwords must match</div>
         </div>
 
-        <div class="d-flex justify-content-center">
-          <input type="submit" class="btn btn-secondary m-2 " value="Register">
+        <div class="form-group">
+          <input type="submit" class="btn btn-outline-dark" value="Registration">
         </div>
-
       </div>
-    </div>
+    </form>
   </div>
 
 </template>
 
 <script>
+import {required, maxLength, minLength, email, sameAs} from 'vuelidate/lib/validators'
+
 export default {
-  name: "Register"
+  name: "Register",
+  data() {
+    return {
+      name: '',
+      surname: '',
+      age: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    }
+  },
+  validations: {
+    name: {
+      required
+    },
+    surname: {
+      required
+    },
+    age: {
+      maxLength:maxLength(2),
+      required
+    },
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6),
+      maxLength: maxLength(12)
+    },
+    confirmPassword: {
+      required,
+      sameAsPassword: sameAs('password')
+    }
+  },
+
+  methods: {
+    submit() {
+      this.$v.$touch();
+      if (this.$v.$pending() || this.$v.$error()) {
+        return
+      }
+    },
+    validationStatus(validation) {
+      return typeof validation != "undefined" ? validation.$error : false
+    }
+  }
 }
 </script>
 
--- src/components/Pages/User.vue
+++ src/components/Pages/User.vue
@@ -1,12 +1,27 @@
 <template>
-<div class="user-container">
-<div style="text-align: center">User page</div>
-</div>
+  <div class="user-container">
+    <div style="text-align: center">User page</div>
+    <button @click="showUser">Show</button>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: "User"
+  name: "User",
+  data() {
+    return {
+      user:''
+    }
+  },
+  methods: {
+    async showUser() {
+      await axios
+        .post('http://127.0.0.1:8000/api/auth/user/1')
+        .then(response => (this.info = response));
+      console.log(this.info.data)
+      }
+    }
 }
 </script>
 
